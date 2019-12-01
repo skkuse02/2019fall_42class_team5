418ae8bddd0901dd998857dd9997ac9eeab8b126
baekjiyeon <whitedelay@gmail.com>
2019-12-01 18:49:03 +0900

    Add global class to get server address

---
+++ se_team5/app/src/main/java/com/example/se_team5/MyGlobal.java
@@ -0,0 +1,10 @@
+package com.example.se_team5;
+
+public class MyGlobal {
+    static private String server_address = "http://14fb4cb6.ngrok.io";
+    static public String getData()
+    {
+        return server_address;
+    }
+
+}
