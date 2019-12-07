f6e560a53190905b81c1acd48d73c95531e88177
Chae hj <julie-.-@hanmail.net>
2019-12-07 19:07:43 +0900

    Change database host

--- Server/routes/login.js
+++ Server/routes/login.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : '192.168.0.79',
+    host     : '115.145.227.249',
     user     : 'seteam5',
     password : 'se55555',
     port     : 3306,
